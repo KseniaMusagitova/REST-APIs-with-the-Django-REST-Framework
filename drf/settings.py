ba5cb069b75f5813367d0af489f6348ee8d824dc
Kseniia Musagitova <kseniamusagitova@gmail.com>
2023-05-05 13:24:08 -0500

    Created another app called frontend / Wrote the code fetches data from a web API and dynamically generates HTML elements to display the data in the browser

--- drf/settings.py
+++ drf/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'app.apps.AppConfig',
     'rest_framework',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
